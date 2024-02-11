c094cd041daceef45e61bfc7febdab5bcdf100b1
Marina <likholetova.marina@gmail.com>
2024-02-11 14:50:40 +0200

    Add task #3

---
+++ homework_2/task3.ts
@@ -0,0 +1,27 @@
+// 3
+// Об’явіть enum Category для зберігання наступних категорій книг: JavaScript, CSS, HTML, TypeScript, Angular.
+// Додайте категорію до об'єктів у функції getAllBooks().
+import { Book } from "./task1";
+
+enum Category {
+    JavaScript,
+    CSS,
+    HTML,
+    TypeScript,
+    Angular
+}
+
+type BookWithCategory = Book & {category: Category[]}
+
+function getAllBooks(): BookWithCategory[] {
+      const books = [
+        { id: 1, title: 'Refactoring JavaScript', author: 'Evan Burchard', available: true, category: [Category.JavaScript] },
+        { id: 2, title: 'JavaScript Testing', author: 'Liang Yuxian Eugene', available: false, category: [Category.JavaScript] },
+        { id: 3, title: 'CSS Secrets', author: 'Lea Verou', available: true, category: [Category.CSS] },
+        { id: 4, title: 'Mastering JavaScript Object-Oriented Programming', author: 'Andrea Chiarelli', available: true, category: [Category.JavaScript] }
+    ];
+
+    return books;
+}
+
+console.log(getAllBooks());
